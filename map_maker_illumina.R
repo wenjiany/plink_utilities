2f9376c0cb0ce0c67acc822eba432bfba5c86c98
wenjiany <wenjiany@users.noreply.github.com>
2024-05-23 15:58:52 -0500

    Update map_maker_illumina.R
    add allele info to map file

--- map_maker_illumina.R
+++ map_maker_illumina.R
@@ -10,8 +10,12 @@ dim(ann_data)
 tail(ann_data)
 
 ann_data$cm <- 0
+ann_data$allele1 <- gsub('\\[', '', gsub('\\/.*', '', ann_data$Alleles))
+ann_data$allele2 <- gsub('\\]', '', gsub('.*\\/', '', ann_data$Alleles))
 
-map_data <- ann_data[, c("Chr", "Name", "cm", "MapInfo")]
+head(ann_data)
+
+map_data <- ann_data[, c("Chr", "Name", "cm", "MapInfo", "allele1", "allele2")]
 
 fwrite(map_data, file="data/infinium-global-diversity-array-8-v1-0_D1.hg19.annotated.map", col.names=FALSE, sep="\t")
 
